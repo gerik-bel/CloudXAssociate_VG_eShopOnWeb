a3b5ed626fdcb41a3585866f52cf8053c3aa587b
Vladimir Gerasimov <vladimir_gerasimov@epam.com>
2023-03-05 18:19:02 +0100

    First commit to the final task

---
+++ src/OrderProcessor/OrderProcessor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace OrderProcessorService;
+
+public static class OrderProcessor
+{
+    private static readonly string EndpointUri = Environment.GetEnvironmentVariable("EndPointUri");
+    private static readonly string PrimaryKey = Environment.GetEnvironmentVariable("PrimaryKey");
+    private static readonly string DatabaseId = "ToDoList";
+    private static readonly string ContainerId = "Items";
+
+    [FunctionName("OrderProcessor")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        CosmosClient cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+        Database database = await cosmosClient.CreateDatabaseIfNotExistsAsync(DatabaseId);
+        Container container = await database.CreateContainerIfNotExistsAsync(ContainerId, "/BuyerId");
+
+        log.LogInformation("OrderProcessor function processed a request.");
+        try
+        {
+            using (StreamReader reader = new StreamReader(req.Body))
+            {
+                var requestBody = await reader.ReadToEndAsync();
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
+                double total = 0;
+                foreach (dynamic item in data.OrderItems)
+                {
+                    total = total + (double)item.Units * (double)item.UnitPrice;
+                }
+                data.TotalPrice = total;
+                data.id = Guid.NewGuid().ToString();
+                var response = await container.CreateItemAsync(data);
+            }
+        }
+        catch (JsonException ex)
+        {
+            log.LogError(ex, ex.Message);
+            return new BadRequestObjectResult("Provide correct JSON format");
+        }
+        log.LogInformation($"Reservation has been created successfully");
+        return new OkObjectResult("Reservation has been created successfully");
+    }
+}
